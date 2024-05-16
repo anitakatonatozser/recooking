c746693cb81b8794a551b16b7a53c753a84d5832
anitakatonatozser <anitakatonatozser@gmail.com>
2024-05-17 00:02:21 +0200

    refaktor for persistance imports

--- src/main/java/com/example/demo/domain/Food.java
+++ src/main/java/com/example/demo/domain/Food.java
@@ -1,10 +1,10 @@
 package com.example.demo.domain;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
--- src/main/java/com/example/demo/dto/RegistrationRequest.java
+++ src/main/java/com/example/demo/dto/RegistrationRequest.java
@@ -1,8 +1,8 @@
 package com.example.demo.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
